998f143b65f302299073205ae97266013cc7669c
Usman Akram <usman.akram@gmail.com>
2017-04-16 18:28:13 +0500

    Products List and Show and Create

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,38 @@
+@extends('master')
+@section('content')
+    <form action="/products" class="form-horizontal" method="post">
+        {{csrf_field()}}
+        {{method_field('POST')}}
+
+        <div class="form-group">
+            <label for="title" class="col-sm-2 control-label">Title</label>
+            <div class="col-sm-10">
+                <input type="text" name="title" class="form-control" id="title" placeholder="Title">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="slug" class="col-sm-2 control-label">Slug</label>
+            <div class="col-sm-10">
+                <input type="text" name="slug" class="form-control" id="slug" placeholder="Slug">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="price" class="col-sm-2 control-label">Price</label>
+            <div class="col-sm-10">
+                <input type="text" name="price" class="form-control" id="price" placeholder="Price">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="description" class="col-sm-2 control-label">Description</label>
+            <div class="col-sm-10">
+                <textarea type="text" name="description" class="form-control" id="description" placeholder="Description">
+                    </textarea>
+            </div>
+        </div>
+
+        <input type="submit" class="btn btn-info">
+
+    </form>
+@endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,4 +1,20 @@
 @extends('master')
 @section('content')
-List Products Here
+    <h1>Products Listings</h1>
+    @foreach($products as $product)
+        <div class="row">
+            <h1>{{$product->title}}</h1>
+            <div class="col-md-6"><a href="/products/{{$product->id}}">Show product</a></div>
+            <div class="col-md-6">{{$product->price}} &pound;</div>
+        </div>
+        <hr>
+    @endforeach
+
+@endsection
+
+@section('sidebar')
+    <h3>Products Table Operations</h3>
+    <ul class="nav nav-pills nav-stacked">
+        <li><a href="/products/create">Create New Product</a></li>
+    </ul>
 @endsection
